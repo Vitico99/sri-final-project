db797df931890b145e1a5336cb4477aea3415bba
Vitico99 <nenevictor99@gmail.com>
2022-06-14 21:19:54 -0400

    Download cranfield dataset and setup black formatter

--- downloads.py
+++ downloads.py
@@ -1,11 +1,9 @@
 import nltk
 import ir_datasets
 
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('wordnet')
+nltk.download("stopwords")
+nltk.download("punkt")
+nltk.download("wordnet")
 
-cranfield = ir_datasets.load('cranfield')
-
-
-
+cranfield = ir_datasets.load("cranfield")
+next(cranfield.docs_iter())
