b94b622462cf20d51e79fb68dfcb13bd3b28ab2b
Vitico99 <nenevictor99@gmail.com>
2022-06-16 17:00:41 -0400

    Update setup requirements and downloads

--- downloads.py
+++ downloads.py
@@ -4,6 +4,7 @@ import ir_datasets
 nltk.download("stopwords")
 nltk.download("punkt")
 nltk.download("wordnet")
+nltk.download("omw-1.4")
 
 cranfield = ir_datasets.load("cranfield")
 next(cranfield.docs_iter())
